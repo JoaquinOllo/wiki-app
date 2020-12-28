18078a00e42e319c86067866290d0bcb1a3f8a05
Ollo <joaquin.ollo@accenture.com>
2020-12-28 01:19:20 -0300

    Agregada clase AnotatedText, que permite extraer de un texto formateado tanto links, como sus descripciones, y también reducirlos omitiendo las explicaciones.

--- Engine/TextOperations.py
+++ Engine/TextOperations.py
@@ -2,10 +2,49 @@ import re
 
 
 class LinkedText:
-    def __init__(self, text, links=[]):
+    def __init__(self, text, links=[], title=""):
+        self.title = title
         self.text = text
         self.links = links
 
+class AnotatedText:
+    def __init__(self, text):
+        self.text = text
+        self.sublinks = []
+        
+        mainLinkRegex = "(?<=<<).+?(?=>>)"
+        regexMainLinkPattern = re.compile (mainLinkRegex)
+        secondaryLinkRegex = "(?<=<).+?(?=>)"
+        regexSecLinkPattern = re.compile (secondaryLinkRegex)
+        fragmentRegex = r'(?<=\[).*?<<.+?>>.*?(?=\])'
+        regexFragmPattern = re.compile (fragmentRegex)
+
+        fragmentMatchesIter = re.finditer(regexFragmPattern, text)
+        
+        for match in fragmentMatchesIter:
+            titleMatch = re.findall(regexMainLinkPattern, match[0])
+            title = titleMatch[0]
+
+            matchSentence = turnFragIntoSentence(match[0])
+
+            linkedText = extractLinks(matchSentence)
+
+            linkedText.title = title
+
+            self.sublinks.append(linkedText)
+
+    def getReducedText(self):
+        
+        newText = self.text
+        fragmentRegex = r'\[.*?<<(.+?)>>.*?\]'
+        regexFragmPattern = re.compile (fragmentRegex)
+
+        replacementFunction = lambda match : "<" + match[1] + ">"
+
+        newText = re.sub(regexFragmPattern, replacementFunction, self.text)
+
+        return newText
+            
 
 def extractLinks(text):
     formattedText = LinkedText("")
@@ -105,8 +144,42 @@ def offsetSlots(operation, existingSlots):
 
     newOperation = re.sub(regexPattern, replacementFunction, operation)
 
+
     return newOperation
         
+def turnFragIntoSentence(text):
+    mainLinkRegex = "<<.+?>>"
+    mainLinkPattern = re.compile(mainLinkRegex)
+
+    textWOMainLink = re.sub(mainLinkPattern, "", text)
+    print(textWOMainLink)
+
+    punctuationMarksRegex = "^[ ,.:;\-!?]*"
+    punctuationMarksPattern = re.compile(punctuationMarksRegex)
+
+    textStartNormalized = re.sub(punctuationMarksPattern, "", textWOMainLink)
+
+    punctuationMarksEndRegex = "[ ,.:;\-!?]*$"
+    punctuationMarksEndPattern = re.compile(punctuationMarksEndRegex)
+
+    textEndNormalized = re.sub(punctuationMarksEndPattern, ".", textStartNormalized)
+
+    textCapitalized = capitalize(textEndNormalized)
+
+    return textCapitalized
+
+def capitalize(text):
+    firstLetterRegex = "(?i)[a-zñÑ]"
+    firstLetterPattern = re.compile(firstLetterRegex)
+    contador = 0
+    newText = ""
+
+    match = re.search(firstLetterPattern, text)
+
+    if match:
+        newText = text[:match.start(0)] + match[0].upper() + text[match.end(0):]
+
+    return newText
 
 
 
@@ -119,3 +192,6 @@ def offsetSlots(operation, existingSlots):
 ##print (casa.text)
 ##print (casa.links)
 ##print(offsetSlots("<1> fue el <2> de mi <3>", 3))
+#casa = AnotatedText("Se encontró con [<<Aurigas>>, <teniente general> del <quinto ejército>]. Cenaron juntos en [el arcón gris].")
+#print (casa.getReducedText())
+#print (turnFragIntoSentence(", <teniente general> del <quinto ejército>"))
