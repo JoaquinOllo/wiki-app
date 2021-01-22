e78588084c00e049dfbabcfc5bdd5f7ecd17f3f6
Ollo <joaquin.ollo@accenture.com>
2021-01-21 23:38:16 -0300

    imported axios through require.js, with the whole index script as a callback. Added constants for getLinks parameters, server url, methods for extracting params out of url and service requests, and a 2nd method for filling in URL parameters. After getting the sought link from the backend, it is passed down to link-display component, where it is shown, for now, unformatted.

--- App/index.js
+++ App/index.js
@@ -1,78 +1,153 @@
-const axios = require('axios').default;
-
-// 1. Define route components.
-// These can be imported from other files
-const Home = { template: '<inicio></inicio>' }
-const Crear = { template: '<creador-links></creador-links>' }
-const Buscar = { template: '<buscador></buscador>' }
-
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
-const routes = [
-  { path: '/', component: Home },
-  { path: '/crear', component: Crear },
-  { path: '/buscar', component: Buscar }
-]
-
-const router = VueRouter.createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-  history: VueRouter.createWebHashHistory(),
-  routes, // short for `routes: routes`
-})
+require(['axios.min'], function (axios) {
+  // 1. Define route components.
+  // These can be imported from other files
+  const Home = { template: '<inicio></inicio>' }
+  const Crear = { template: '<creador-links></creador-links>' }
+  const Buscar = { template: '<buscador></buscador>' }
 
 
-const app = Vue.createApp({});
+  const server = "http://127.0.0.1:5000/";
+  
+  const getLinksParams = {
+    URL: "links/alias/<linkSought>",
+    method: "GET"
+  };
 
-app.component('buscador', {
-  template: `
+  const extractParams = (text) => {
+    const regExp = RegExp("(?<=<).+?(?=>)", "gi");
+    params = Array.from(text.matchAll(regExp));
+    return params;
+  };
+
+  /**
+   * 
+   * @param {String} URL a string with the unformatted URL with empty params
+   * @param {Array} paramsArray paramsArray should contain field-value pairings
+   *
+   */
+  const fillinURLParams = (URL, paramsArray) => {
+    let newURL = URL;
+    paramsArray.forEach(param => {
+      let regExpPattern = `<${param.field}>`;
+      let regExp = RegExp(regExpPattern, "gi");
+      newURL = newURL.replace(regExp, param.value)
+    });
+    return newURL;
+  }
+
+  // 2. Define some routes
+  // Each route should map to a component.
+  // We'll talk about nested routes later.
+  const routes = [
+    { path: '/', component: Home },
+    { path: '/crear', component: Crear },
+    { path: '/buscar', component: Buscar }
+  ]
+
+  const router = VueRouter.createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: VueRouter.createWebHashHistory(),
+    routes, // short for `routes: routes`
+  })
+
+
+  const app = Vue.createApp({});
+
+  app.component('buscador', {
+    data() {
+      return {
+        link: "hola"
+      }
+    },
+    computed: {
+      serviceParams(){
+        return getLinksParams;
+      }
+    },
+    watch: {
+      link(newLink, oldLink){
+        console.log (newLink)
+        console.log (oldLink)
+      }
+    },
+    methods: {
+      buscar(event) {
+        let params = extractParams(this.serviceParams.URL);
+        let paramsArray = []
+        params.forEach(param => {
+          paramsArray.push({"field": param, "value": event})
+        });
+        axios({
+          method: this.serviceParams.method,
+          url: server + fillinURLParams(this.serviceParams.URL, paramsArray)
+        })
+          .then(function (res) {
+            $vm.data.link = res.data.links
+            console.log ($vm.data.link)
+          })
+          .catch(function (err) {
+            console.log(err)
+          })
+      }
+    },
+    template: `
     <div>
-      <buscador-input></buscador-input>
-      <link-display></link-display>
+      <buscador-input v-on:buscar="buscar($event)"></buscador-input>
+      <link-display v-bind:link="link"></link-display>
     </div>`
-})
+  })
 
-app.component('buscador-input', {
-  data() {
-    return {
-      busqueda: ""
-    }
-  },
-  methods: {
-    buscar () {
-      this.$emit
-    }
-  },
-  template: `
+  app.component('buscador-input', {
+    data() {
+      return {
+        busqueda: ""
+      }
+    },
+    methods: {
+      buscar() {
+        this.$emit("buscar", this.busqueda);
+      }
+    },
+    template: `
     <div>
       <p>{{busqueda}}</p>
       <label for="busqueda">Ingrese un nombre del link</label>
       <input v-model="busqueda" type="text" name="busqueda">
-      <button v-on:click="buscar()" >Buscar</button>
+      <button v-on:click="buscar" >Buscar</button>
     </div>`
-})
+  })
 
-app.component('link-display', {
-  template: `
+  app.component('link-display', {
+    props:{
+      link: Object
+    },
+    watch: {
+      link(newLink, oldLink){
+        console.log (newLink)
+        console.log (oldLink)
+      }
+    },
+    template: `
     <div>
-      Aquí el link!
+      {{link}}
     </div>`
-})
+  })
 
-app.component('creador-links', {
-  template: `
+  app.component('creador-links', {
+    template: `
     <div>
       Aquí el creador de links
     </div>`
-})
+  })
 
-app.component('inicio', {
-  template: `
+  app.component('inicio', {
+    template: `
     <div>
       Inicio de la aplicación
     </div>`
-})
+  })
 
-app.use(router);
+  app.use(router);
 
-app.mount('#app');
+  app.mount('#app');
+});
